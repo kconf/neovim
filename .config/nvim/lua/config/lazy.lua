7de9f4bcfae99832d3519cd04501d218473a0ece
Jianwei Han <hanjianwei@gmail.com>
2026-01-05 17:19:20 +0800

    feat: optimize config

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -19,9 +19,9 @@ require("lazy").setup({
     { import = "plugins" },
   },
   defaults = {
-    -- By default, only LazyVim plugins will be lazy-loaded. Your custom plugins will load during startup.
-    -- If you know what you're doing, you can set this to `true` to have all your custom plugins lazy-loaded by default.
-    lazy = false,
+    -- Enable lazy-loading by default for better performance
+    -- Plugins can override this by setting lazy = false
+    lazy = true,
     -- It's recommended to leave version=false for now, since a lot the plugin that support versioning,
     -- have outdated releases, which may break your Neovim install.
     version = false, -- always use the latest git commit
@@ -35,8 +35,8 @@ require("lazy").setup({
       disabled_plugins = {
         "gzip",
         -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
+        "matchparen", -- disable if you don't need bracket highlighting
+        "netrwPlugin", -- disable netrw if using file explorer plugins
         "tarPlugin",
         "tohtml",
         "tutor",
