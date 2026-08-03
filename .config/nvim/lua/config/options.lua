15bd22cb1e1befb0c970387b736a6116570b158d
Jianwei Han <hanjianwei@gmail.com>
2026-08-03 09:55:35 +0800

    feat: add PREFIX env support

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -5,3 +5,16 @@ vim.opt.conceallevel = 0
 vim.opt.spelllang = { "en", "cjk" }
 
 vim.g.autoformat = false
+
+if vim.env.PREFIX then
+  local bin = vim.env.PREFIX .. "/bin"
+
+  -- Termux executables
+  if not vim.env.PATH:find(bin, 1, true) then
+    vim.env.PATH = bin .. ":" .. vim.env.PATH
+  end
+
+  -- Treesitter compiler
+  vim.env.CC = bin .. "/clang"
+  vim.env.CXX = bin .. "/clang++"
+end
